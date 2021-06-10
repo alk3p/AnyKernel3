146ee5be9a12a984167cebe6ffb9b5b9b5bc6a13
Park Ju Hyung <qkrwngud825@gmail.com>
2021-06-10 11:55:15 +0800

    ak3: Use the provided dtb

--- anykernel.sh
+++ anykernel.sh
@@ -1,6 +1,10 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
+# Set up working directory variables
+test "$home" || home=$PWD;
+split_img=$home/split_img;
+
 ## AnyKernel setup
 # begin properties
 properties() { '
@@ -45,8 +49,17 @@ fi
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+if [ ! -f $home/source/Image.gz ] || [ ! -f $home/source/dtb ]; then
+  ui_print " " "This zip is corrupted! Aborting..."; exit 1;
+else
+  mv $home/source/Image.gz $home/Image.gz;
+fi
+
 ## AnyKernel boot install
 dump_boot;
+
+# Use the provided dtb
+mv $home/source/dtb $home/split_img/;
 
 write_boot;
 ## end boot install
